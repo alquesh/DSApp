70d4b83317a1e7306ed60fa0cfc1b0913800bdd6
alquesh <mapspam@gmx.de>
2015-02-21 15:59:22 +0100

    Update modifier.java

--- src/booltest/modifier.java
+++ src/booltest/modifier.java
@@ -1,1 +1,17 @@
-
+public class modifier{
+  public int wert;
+  public String descriptor;
+  
+  public modifier(int wert, String descriptor){
+    this.wert = wert;
+    this.descriptor = descriptor;
+  }
+  
+  public modifier(modifier that){
+    this(that.wert, that.descriptor);
+  }
+  
+  public String toString(){
+    return descriptor+": "+wert;
+  }
+}
