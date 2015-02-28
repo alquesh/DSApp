155027ca30432242cecfb44a6a2257b54450e8fd
alquesh <mapspam@gmx.de>
2015-02-28 17:55:10 +0100

    Update timedModifier.java

--- src/booltest/DSApp/timedModifier.java
+++ src/booltest/DSApp/timedModifier.java
@@ -1,1 +1,20 @@
 
+package DSApp;
+
+public class timedModifier extends modifier{
+  public int turnsremaining;
+  
+  
+  public timedModifier(int wert, String descriptor, int turns){
+    super(wert, descriptor);
+    turnsremaining = turns;
+  }
+  
+  public timedModifier(timedModifier that){
+    this(that.wert, that.descriptor, that.turns);
+  }
+  
+  public String toString(){
+    return descriptor+": "+wert+" Turns remaining: "+turnsremaining;
+  }
+}
